d54e1daa4b89347a8d8860f331ea4828bd0405af
Faisal Ahammad <faisalahammad24@gmail.com>
2022-06-16 23:20:33 +0530

    Added - All variables

---
+++ main.js
@@ -0,0 +1,15 @@
+// variables
+// Buttons selector
+const getStartedBtn = document.getElementById("get-started-btn");
+const player1Btn = document.getElementById("player1-btn");
+const player2Btn = document.getElementById("player2-btn");
+const resetBtn = document.getElementById("reset-btn");
+
+// Score
+const luckyNum = document.getElementById("lucky-number");
+const player1Num = document.getElementById("player1-number");
+const player2Num = document.getElementById("player2-number");
+
+let finalNumber = "";
+let player1Number = "";
+let player2Number = "";
